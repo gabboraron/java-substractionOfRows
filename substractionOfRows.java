61e0cfa792287c40e85f4029e4a6f9fa56a911ff
gabboraron <gabboraron@gmail.com>
2017-11-16 14:05:10 +0100

    Add files via upload

--- substractionOfRows.java
+++ substractionOfRows.java
@@ -9,6 +9,7 @@ public class substractionOfRows{
 		} else {
 			try{
 				substractionWithoutBufferReader(args[0]);
+				//substractionWithBufferReader(args[0]);
 			} catch (IOException e){
 				System.err.println("IO ERROR");
 				e.printStackTrace();
@@ -38,6 +39,31 @@ public class substractionOfRows{
 					}
 					
 					System.out.println(Arrays.toString(sub));
+					System.out.println(" ");
+				}
+			}
+		}
+	}
+	
+	public static void substractionWithBufferReader(String input)
+	throws IOException{
+		File inFile = new File(input);
+		try( BufferedReader br = new BufferedReader(new FileReader(inFile));){
+			String line;
+			for(line = br.readLine(); line!=null; line=br.readLine()){
+				String line1 = line;
+				line = br.readLine();
+				if(line!=null){
+					String[] res1 = line1.split(",");
+					String[] res2 = line.split(",");
+					int[] tmp = new int[res1.length];
+					for(int i=0; i<res1.length; ++i){
+						tmp[i] = Integer.parseInt(res1[i]) - Integer.parseInt(res2[i]);
+					}
+					System.out.println(Arrays.toString(res1));
+					System.out.println(Arrays.toString(res2));
+					System.out.println("----");
+					System.out.println(Arrays.toString(tmp));
 					System.out.println(" ");
 				}
 			}
